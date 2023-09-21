6a603c82105d3e4bc2ce96353d0f74b9cd1872ca
lernEarnBurn <lerners124@gmail.com>
2023-09-21 16:10:11 +0300

    set up some routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+const userController = require('../controllers/userController')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { user: req.user });
 });
+
+router.get('/sign-up', function(req, res, next) {
+  res.render('signUp')
+})
+
+router.post('/sign-up', userController.createUser)
 
 module.exports = router;
