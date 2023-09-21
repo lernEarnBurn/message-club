ecc202a270ada03f69869a5d629bb8f6879e8a94
lernEarnBurn <lerners124@gmail.com>
2023-09-21 14:46:20 +0300

    added password field

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const userSchema = new Schema({
     firstname: {type: String, required: true},
     lastname: {type: String, required: true},
     email: {type: String, required: true},
+    password: {type: String, required: true},
     membership: {type: String, enum: ['none', 'regular', 'admin'], default: 'none'},
 })
 
