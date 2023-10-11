e461129120b86fbf6f22464a403d17d565eaca22
lernEarnBurn <lerners124@gmail.com>
2023-10-11 15:01:41 +0300

    added logout abilities

--- routes/auth.js
+++ routes/auth.js
@@ -3,12 +3,18 @@ var router = express.Router();
 const passport = require('passport')
 
 
-
-
-
 router.post('/log-in', passport.authenticate("local", {
     successRedirect: "/",
     failureRedirect: "/"
 }))
+
+router.get("/log-out", (req, res, next) => {
+    req.logout((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/");
+    });
+});
 
 module.exports = router;
